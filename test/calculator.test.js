e01edb511d8c7438b9a2402e2c2f51b7bb3ba6fa
Ruchita Makwana <ruchitahm@gmail.com>
2024-11-26 14:31:28 -0800

    Handle Multiple Numbers test case added

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -11,3 +11,8 @@ test('returns the number itself for a single number', () => {
 test('returns the sum of two numbers separated by a comma', () => {
   expect(add('1,2')).toBe(3);
 });
+
+test('returns the sum of multiple numbers', () => {
+    expect(add('1,2,3,4')).toBe(10);
+  });
+  
